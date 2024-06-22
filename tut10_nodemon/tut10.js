81fb63ccbf47ac6413cf50a3c03d369a4fc062b7
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-06-22 11:08:28 +0530

    install nodemon and how to use

---
+++ tut10_nodemon/tut10.js
@@ -0,0 +1,7 @@
+//this tutorial we discuss what is nodemon packege/module and how we use
+//nodemon is a tool that helps develop Node.js based applications by automatically restarting the node application when file changes in the directory are detected.
+// command for install --> npm i nodemon
+// run for command --> nodemon file_name
+
+console.log("this is tu10")
+console.log("hai ravi")
