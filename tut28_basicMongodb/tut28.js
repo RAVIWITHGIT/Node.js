4f441b853a6c04b1ed25b4b11d2349b10f5aec17
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-07-09 22:29:29 +0530

    basic command of mongdb databas

---
+++ tut28_basicMongodb/tut28.js
@@ -0,0 +1,12 @@
+/* *********************************** basic command for mongodb databas
+1. show all databas --> show dbs
+2. create new databas --> use database_name
+3. create collection --> db.createCollection('collection_name')
+4. show all collection of specific databas --> show collections
+5. delete collectin in databas --> db.collection_name.drop()
+6. delete database where i present --> db.dropDatabase()
+
+
+
+
+*/
