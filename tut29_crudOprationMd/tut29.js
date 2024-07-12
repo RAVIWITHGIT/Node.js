0465214001be7a1cb40ecc8de17207bc2176f7b7
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-07-12 11:41:29 +0530

    mongodb crud opration

---
+++ tut29_crudOprationMd/tut29.js
@@ -0,0 +1,9 @@
+/********************************* mongodb crud opration *******************************
+1.create database --> use database_name
+2.insert data in collection --> db.collection_name.insertOne({field:value}),
+3. show all data --> db.collection_name.find()
+4. update data in collection --> db.collection_name.updateOne({field which you want update},{$set:{field:value}})
+5. delete data from collection --> db.collection_name.deleteOne({field which you want delete})
+
+
+*/
